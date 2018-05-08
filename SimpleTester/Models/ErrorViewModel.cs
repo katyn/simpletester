237954789e955b8880b441bfad08e700961845d0
kprice <katy.price@cuc.org>
2018-05-08 14:11:15 -0500

    Add project files.

---
+++ SimpleTester/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleTester.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
